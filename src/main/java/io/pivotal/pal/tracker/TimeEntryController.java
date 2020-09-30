ce014d584e48024f389ba3b6954b4805cd72ac7c
Chris Bredernitz <cbreder3@ford.com>
2020-09-30 16:01:20 +0000

    passing Controller and API test for create method

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,16 +1,29 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/time-entries")
 public class TimeEntryController {
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
 
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
     }
 
-    public ResponseEntity create(TimeEntry timeEntryToCreate) {
-        return null;
+    @PostMapping
+    public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntryCreated = this.timeEntryRepository.create(timeEntryToCreate);
+
+        return new ResponseEntity<>(timeEntryCreated, HttpStatus.CREATED);
     }
 
     public ResponseEntity<TimeEntry> read(long timeEntryId) {
